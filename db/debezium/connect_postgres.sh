f8d8a25b19307362991260ce81811b91d0d57122
Mathias Darr <mddarr@gmail.com>
2020-04-18 21:19:25 -0700

    deleat the blight

--- db/debezium/connect_postgres.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
- docker exec -it db psql 'dbname=postgres user=postgres options=--search_path=inventory'
--- db/initDB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker cp db/customers.csv usersDB:/customers.csv
-docker cp db/address.csv usersDB:/address.csv
-docker cp db/populate.sql.sql usersDB:/populate.sql
-docker exec usersDB psql -d usersdb -U database1_role -f populate.sql
